6f3abc77cf18bfdc456cbd5f6f76bce6b3c8f47d
dimru <dimru.tarekegn@gmail.com>
2022-03-09 20:50:14 -0800

    string output by using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /**
- * main: prints out a string in puts.
- * Return: 0 if success
+ * main - Entry point
+ * 
+ * Return: Always 0 Success
  */
 int main(void)
 {
-	puts("\"Programming is like a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
