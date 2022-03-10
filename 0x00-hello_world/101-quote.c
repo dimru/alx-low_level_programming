31d42b148d4425c9bc3f339c7ef735851adef858
dimru <dimru.tarekegn@gmail.com>
2022-03-09 22:51:31 -0800

    using write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char n[] = "and that piece of art is useful\" - Dora korpar, 2015-10-19";
+	fprintf(stderr, "%s\n", n);
 	return (1);
 }
