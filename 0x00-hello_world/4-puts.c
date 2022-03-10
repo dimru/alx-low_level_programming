7b2f27f1079624876353cfc918232a66f4ec15e0
dimru <dimru.tarekegn@gmail.com>
2022-03-09 20:45:18 -0800

    the first puts code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main: prints out a string in puts.
+ * Return: 0 if success
+ */
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+}
