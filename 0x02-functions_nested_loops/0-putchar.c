fa326b338d7bfbcfbc931a0bdd8096fbab888418
vagrant <vagrant@kali>
2022-03-15 08:32:37 -0400

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,10 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c = "_putchar";
+
+	_putchar(c);
+	c++;
 	_putchar('\n');
 
 	return (0);
